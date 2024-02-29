42906abc7f09d928a4e093b084b1d5f6d61c53b6
Randika Hasheen <rhasheen@isa.ae>
2024-02-29 09:20:39 +0530

    Inititial Commit

---
+++ HostedServiceSample/ProcessWorker.cs
@@ -0,0 +1,16 @@
+﻿public class ProcessWorker : BackgroundService
+{
+    private ILogger<ProcessWorker> _logger;
+    public ProcessWorker(ILogger<ProcessWorker> logger)
+    {
+        _logger = logger;
+    }
+    protected async override  Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Background Service is Runing at : {time}", DateTimeOffset.Now);
+            await Task.Delay(1000,stoppingToken);
+        }
+    }
+}
